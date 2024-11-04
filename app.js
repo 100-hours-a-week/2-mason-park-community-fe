47a1bc89cf82e12e50a6a0ee59cb32539a372642
pmsu2007 <pmsu2007@naver.com>
2024-11-04 16:19:43 +0900

    feat : express로 웹 서버 구축

---
+++ app.js
@@ -0,0 +1,61 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import express from 'express';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// 로그인 페이지
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+})
+
+// 회원가입 페이지
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'register.html'));
+})
+
+// 글 목록 페이지
+app.get('/posts', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'postList.html'));
+})
+
+// 글 상세 페이지
+app.get('/posts/:id', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'postDetail.html'));
+})
+
+// 글 작성 페이지
+app.get('/posts/write', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'postWrite.html'));
+})
+
+// 글 수정 페이지
+app.get('/posts/modify', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'postWrite.html'));
+})
+
+// 회원 정보 수정 페이지
+app.get('/users/:id/setting', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'userEdit.html'));
+})
+
+// 비밀번호 변경 페이지
+app.get('/users/:id/password', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'passwordEdit.html'));
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
